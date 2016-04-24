37a33d5c70353a4647fd9b6457330b9ef90070eb
bopes <clcblee@gmail.com>
2016-04-24 10:09:43 -0700

    Add cell highlighting and #findBottomColumnSpace

--- public/js/application.js
+++ public/js/application.js
@@ -2,19 +2,18 @@ $(document).ready(function() {
 
   var turnCounter = 0
 
+  $('.space').hover(
+    function(){
+      var $bottomColumnSpace = findBottomColumnSpace($(this))
+      $bottomColumnSpace.css('background-color', 'white')
+    }, function(){
+      var $bottomColumnSpace = findBottomColumnSpace($(this))
+      $bottomColumnSpace.css('background-color', '#5ed7d2')
+    })
+
+
   $('.space').click(function(){
-
-    var row = "." + $(this).attr('class').split(" ")[1]
-    var column = "." + $(this).attr('class').split(" ")[2]
-
-    for ( var i = 1; i < 8; i++) {
-      if ($(column + ".row" + i).hasClass('empty')) {
-        bottomRow = ".row" + i
-        break
-      }
-    }
-    var $bottomColumnSpace = $(column + bottomRow)
-
+    var $bottomColumnSpace = findBottomColumnSpace($(this))
     if ($(this).hasClass('empty')) {
       if ( turnCounter % 2 === 0 ) {
         $bottomColumnSpace.css('background-color', 'red')
@@ -42,4 +41,17 @@ $(document).ready(function() {
   })
 
 });
+
+
+function findBottomColumnSpace(space) {
+  var row = "." + space.attr('class').split(" ")[1]
+  var column = "." + space.attr('class').split(" ")[2]
+  for ( var i = 1; i < 8; i++) {
+    if ($(column + ".row" + i).hasClass('empty')) {
+      bottomRow = ".row" + i
+      break
+    }
+  }
+  return $(column + bottomRow)
+}
 
