d3c802ee5c46eef38e0219645093beff28a000d8
bopes <clcblee@gmail.com>
2016-04-23 17:40:55 -0700

    Prevent changing a filled space

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
+
   var turnCounter = 0
+
   $('.space').click(function(){
-    if ( turnCounter % 2 === 0 ) {
-      $(this).css('background-color', 'red')
-      turnCounter += 1
-    } else {
-      $(this).css('background-color', 'black')
-      turnCounter += 1
+
+    var row = $(this).attr('class').split(" ")[1]
+    var column = $(this).attr('class').split(" ")[2]
+
+    if ($(this).hasClass('empty')) {
+      if ( turnCounter % 2 === 0 ) {
+        $(this).css('background-color', 'red')
+        $(this).removeClass('empty')
+        $(this).addClass('filled')
+        turnCounter += 1
+      } else {
+        $(this).css('background-color', 'black')
+        $(this).removeClass('empty')
+        $(this).addClass('filled')
+        turnCounter += 1
+      }
     }
+
   })
 });
 
