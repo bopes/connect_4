ec25e4614bfd178316fbb8c49dd3f073e89fe1cc
bopes <clcblee@gmail.com>
2016-04-24 21:18:19 -0700

    Fix row win condition bug

--- public/js/application.js
+++ public/js/application.js
@@ -5,6 +5,16 @@ $(document).ready(function() {
   function turnColor(){ if (turnCounter % 2 === 0) { return 'red' } else { return 'black' } }
   function hoverColor(){ if (turnColor() === 'red') { return 'pink' } else { return 'grey' } }
 
+  var rows = []
+  for ( i=0; i<7; i++ ) {
+    var emptyRow = []
+    for ( i=0; i<7; i++ ) {
+      emptyRow.push("_")
+    }
+    rows.push(emptyRow)
+  }
+
+
   $('.header').hover(
     function(){
       var $bottomColumnSpace = findBottomColumnSpace($(this))
@@ -22,7 +32,35 @@ $(document).ready(function() {
       colorSpace($bottomColumnSpace, turnColor())
       $bottomColumnSpace.effect( "bounce", { times: 3 }, 500 )
       fillSpace($bottomColumnSpace)
+
+
+
+      var row = findRow($bottomColumnSpace).slice(-1)
+      var column = findColumn($bottomColumnSpace).slice(-1)
+
+      console.log("Row: " + row)
+      console.log("Column: " + column)
+
+      if ( turnColor() === 'red' ) {
+
+
+        rows[row - 1][column - 1] = "R"
+
+
+
+
+      } else { rows[row - 1][column - 1] = "B" }
+
+      for ( i=0; i < 7; i++){
+        console.log(rows[i])
+      }
+
+
+
+      checkRowWins(rows)
+
       turnCounter += 1
+
     }
   })
 
@@ -34,9 +72,17 @@ $(document).ready(function() {
 
 });
 
+function findRow(space){
+  return space.attr('class').split(" ")[1]
+}
+
+function findColumn(space){
+  return space.attr('class').split(" ")[2]
+}
+
 function findBottomColumnSpace(space) {
-  var row = "." + space.attr('class').split(" ")[1]
-  var column = "." + space.attr('class').split(" ")[2]
+  var row = "." + findRow(space)
+  var column = "." + findColumn(space)
   for ( var i = 1; i < 8; i++) {
     if ($(column + ".row" + i).hasClass('empty')) {
       bottomRow = ".row" + i
@@ -63,4 +109,17 @@ function fillSpace(space){
 function clearSpaces(spaces){
   spaces.addClass('empty')
   spaces.removeClass('filled')
+}
+
+function checkAllWins(containers){
+  checkRowWins(containers)
+  // checkColumnWins(containers)
+}
+
+function checkRowWins(containers){
+  for ( i = 0; i < containers.length; i++ ) {
+    var container = containers[i].join("")
+    if (/RRRR/.test(container)) { console.log("Red wins") }
+      else if (/BBBB/.test(container)) { console.log("Black wins") }
+  }
 }
